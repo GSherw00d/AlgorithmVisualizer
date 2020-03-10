06218b4400b981d55b47f2b860b201806c765792
Geoff Sherwood <GeoffSherwood1@gmail.com>
2020-03-10 18:49:40 +0000

    initial changes

---
+++ myapp/src/components/Grid.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Grid extends Component {
+  render() {
+    return (
+      <div>
+        <p>Whaddup</p>
+      </div>
+    );
+  }
+}
+
+export default Grid;
