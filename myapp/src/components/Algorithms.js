7c42ade62e554fe367272d07cae23150b411e0d5
Geoff Sherwood <GeoffSherwood1@gmail.com>
2020-03-18 08:05:43 +0000

    Start of Djikstras

--- myapp/src/components/Algorithms.js
+++ myapp/src/components/Algorithms.js
@@ -1,12 +1,38 @@
 //return the visited nodes in order in an array.
 //return the shortest path in order
 export function algorithm(algorithmThatisActive, currentNodes) {
-  let a = [];
-  if (algorithmThatisActive === "djikstras")
-    for (let i = 0; i < currentNodes.length; i++) {
-      a.push(currentNodes[i].id);
+  const minTotalCosts = {};
+  const prevNodes = {};
+  const MinPQ = {};
+  const visited = [];
+
+  //find start node
+  const startNodeId = currentNodes.find(node => {
+    return node.type === "SNODE";
+  }).id;
+
+  const FinishNodeId = currentNodes.find(node => {
+    return node.type === "FNODE";
+  }).id;
+  //Need to put in error handling here
+
+  const nodesGoingIntoDijkstras = currentNodes.filter(
+    node => node.type !== "WALL"
+  );
+  console.log(nodesGoingIntoDijkstras);
+  minTotalCosts[startNodeId] = 0; //Set start node cost to 0
+  MinPQ[startNodeId] = minTotalCosts.startNodeId; //Add start node to minPQ
+
+  for (let i = 0; i < nodesGoingIntoDijkstras.length; i++) {
+    if (nodesGoingIntoDijkstras[i].id !== startNodeId) {
+      //for all nodes other than start
+      minTotalCosts[nodesGoingIntoDijkstras[i].id] = Infinity; //set the distance to infinity
     }
-  return a;
+  }
+
+  console.log(minTotalCosts);
+
+  return ["1_1"];
   //now all I need to do is to learn djistras algorithm
   //need to check for a start and a finish.
   //Lets assume they are there for the time being
