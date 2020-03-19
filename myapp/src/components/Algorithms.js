44ddd855ba1fa3e0e5900c46a421d846366394d0
Geoff Sherwood <GeoffSherwood1@gmail.com>
2020-03-19 21:35:05 +0000

    Todays Work

--- myapp/src/components/Algorithms.js
+++ myapp/src/components/Algorithms.js
@@ -1,44 +1,65 @@
 //return the visited nodes in order in an array.
 //return the shortest path in order
 export function algorithm(algorithmThatisActive, currentNodes) {
-  const minTotalCosts = {}; // map of node and int {id : "x", distance: x}
+  const minTotalCosts = {}; // map of node and int {id : "distance"}
   const prevNodes = {}; // map of node and node {id: "id"}
-  const minPQ = []; // list of nodes
+  let minPQ = []; // list of objects node and in [{id: x, distance: x}]
   const visited = []; //array of nodes
 
   let removeSmallest = minPQ => {
     let smallestDistance = Infinity;
     let index = -1;
     let node = {};
-    console.log(minPQ);
     for (let i = 0; i < minPQ.length; i++) {
-      if (this.minPQ[i].distance < smallestDistance) {
-        smallestDistance = this.minPQ[i].distance;
+      if (minPQ[i].distance < smallestDistance) {
+        smallestDistance = minPQ[i].distance;
         index = i;
         node = minPQ[i];
       }
     }
     minPQ.splice(index, 1);
-    return node.id;
+    //need to return the new minPQ as well
+    return node;
   };
 
   let nodesNeighbours = (newSmallest, nodesGoingIntoDijkstras) => {
-    //I haven't tested this yet;
-    let rcNS = newSmallest.split("_"); //use a for loop
+    let rcNS = newSmallest.id.split("_"); //use a for loop
     let n = [];
     for (let i = 0; i < nodesGoingIntoDijkstras.length; i++) {
-      let rc = nodesGoingIntoDijkstras[i].split("_");
-      if (Math.abs(rcNS[0] - rc[0]) === 1 && Math.abs(rcNS[1] - rc[1])) {
+      let rc = nodesGoingIntoDijkstras[i].id.split("_");
+
+      if (
+        (Math.abs(parseInt(rcNS[0]) - parseInt(rc[0])) === 1 ||
+          Math.abs(parseInt(rcNS[1]) - parseInt(rc[1])) === 1) &&
+        Math.abs(
+          parseInt(rcNS[0]) -
+            parseInt(rc[0]) +
+            parseInt(rcNS[1]) -
+            parseInt(rc[1])
+        ) === 1 &&
+        (Math.abs(parseInt(rcNS[0]) - parseInt(rc[0])) === 0 ||
+          Math.abs(parseInt(rcNS[1]) - parseInt(rc[1])) === 0)
+      ) {
         n.push(nodesGoingIntoDijkstras[i]);
       }
     }
     return n;
   };
 
+  let containsNeighbour = (visited, node) => {
+    // visited as array node as string
+    if (visited.indexOf(node.id) !== -1) {
+      return true;
+    }
+    return false;
+  };
+
   //find start node
-  const startNodeId = currentNodes.find(node => {
-    return node.type === "SNODE";
-  }).id;
+  const startNodeId = currentNodes
+    .find(node => {
+      return node.type === "SNODE";
+    })
+    .id.toString();
 
   const finishNodeId = currentNodes.find(node => {
     return node.type === "FNODE";
@@ -49,9 +70,8 @@ export function algorithm(algorithmThatisActive, currentNodes) {
     node => node.type !== "WALL"
   );
 
-  console.log(nodesGoingIntoDijkstras);
   minTotalCosts[startNodeId] = 0; //Set start node cost to 0
-  minPQ[startNodeId] = minTotalCosts.startNodeId; //Add start node to minPQ
+  minPQ.push({ id: startNodeId, distance: 0 }); //Add start node to minPQ
 
   for (let i = 0; i < nodesGoingIntoDijkstras.length; i++) {
     if (nodesGoingIntoDijkstras[i].id !== startNodeId) {
@@ -67,22 +87,27 @@ export function algorithm(algorithmThatisActive, currentNodes) {
 
     for (let i = 0; i < neighbours.length; i++) {
       //check neighbours
-      if (!visited.containsNeighbour()) {
-        //GOT TO HERE
+      if (!containsNeighbour(visited, neighbours[i])) {
         //check if the neighbour has been visited
-        let altPath =
-          minTotalCosts.get(newSmallest) + distance(newSmallest, neighbours); //make a path, check distance
-        if (altPath < minTotalCosts.get(neighbours)) {
+        let altPath = minTotalCosts[newSmallest.id] + 1; //make a path, check distance
+        console.log(altPath);
+        if (altPath < minTotalCosts[neighbours[i]]) {
           // Check if new path better
-          minTotalCosts.put(neighbours, altpath); //Update path length for neighbour
-          prevNodes.put(neighbours, newSmallest); //Update previous node to node we just removed
+          minTotalCosts[neighbours[i]] = altPath; //Update path length for neighbour
+          prevNodes[neighbours[i]] = newSmallest.id; //Update previous node to node we just removed
 
-          minPQ.decreasePriority(neighbours, altPath); //Update priority in minPQ
+          for (let j = 0; j < minPQ.length; i++) {
+            //Update priority in minPQ
+            if (minPQ[j].id === neighbours[0]) {
+              minPQ[j] = { id: neighbours[0], distance: altPath };
+            }
+          }
         }
       }
     }
   }
 
+  console.log(minTotalCosts);
   // then we would return the total cost and the previous node.
   //However what about the list of visited nodes, i reckon we just return that straight as is. Bet as each node is the same cost we can return the visited
   //nodes list up to the finish node.
